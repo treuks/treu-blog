59177b019720e8cb7d310e067e6ba88b21871946
Isabelle <ks2225@protonmail.com>
2024-02-21 02:05:52 +0200

    add some stuff

--- posts/first-blog-post.md
+++ posts/first-blog-post.md
@@ -1,6 +1,6 @@
 ---
 title: First blog post!
-published_at: 2022-11-04T15:00:00.000Z
+published_at: 2024-02-21T00:04:30.502Z
 snippet: just a test article
 ---
 
--- routes/post/[article].tsx
+++ routes/post/[article].tsx
@@ -20,6 +20,7 @@ export default function ArticlePage(props: PageProps<Post>) {
             <main class="blog-background">
                 <Head>
                     <style dangerouslySetInnerHTML={{__html: CSS}}/>
+                    <title>TreuKS - {post.title}</title>
                     <style>
                     @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap')
                     </style>
