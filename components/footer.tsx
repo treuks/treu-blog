450ed8689977314b60ddc139264e72f15a546b17
Anny <ks2225@protonmail.com>
2024-11-21 21:17:28 +0200

    ✨ Footer: Restyle

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,24 @@ export default function Footer() {
     return (
         <>
             <div class="footer">
-                <p>Made with 🍵 by treuks</p>
+                <div class="footer-content">
+                    <p>
+                        Made with{" "}
+                        <span>
+                            <img
+                                src="/Icons/serenityos_tea_pot.png"
+                                alt="teapot"
+                            />
+                        </span>{" "}
+                        by treuks
+                    </p>
+                    <a
+                        class="generic-link"
+                        href="https://github.com/treuks/treu-blog"
+                    >
+                        Source code
+                    </a>
+                </div>
             </div>
         </>
     );
